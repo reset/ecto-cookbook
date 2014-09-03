889df1baf76604c9b64bba43fc345a2324772077
Jamie Winsor <jamie@vialstudios.com>
2014-09-02 18:01:40 -0700

    version bump 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "jamie@vialstudios.com"
 license          "Apache 2.0"
 description      "Installs, configures, and provides LWRPs for components of Ecto"
 long_description "Installs, configures, and provides LWRPs for components of Ecto"
-version          "0.1.1"
+version          "0.2.0"
 
 depends "elixir"
 depends "libarchive"
