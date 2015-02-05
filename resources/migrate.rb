f94196f72b49ff927b206d256c2fbb6b7ae23267
Jamie Winsor <jamie@vialstudios.com>
2015-02-05 14:08:25 -0800

    update copyright license

--- resources/migrate.rb
+++ resources/migrate.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: ecto
 # Resource:: migrate
 #
-# Copyright (C) 2014 Jamie Winsor
+# Copyright (C) 2014-2015 Jamie Winsor
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
