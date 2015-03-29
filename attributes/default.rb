772aa7b713b194199c46af1dcbef98466fc0c222
Jamie Winsor <jamie@vialstudios.com>
2015-03-29 12:54:00 -0700

    bump default installed version of migrator to 0.5.0

--- attributes/default.rb
+++ attributes/default.rb
@@ -18,5 +18,5 @@
 #
 
 default[:ecto][:migrator][:repo]         = "reset/migrator"
-default[:ecto][:migrator][:release]      = "0.4.2"
+default[:ecto][:migrator][:release]      = "0.5.0"
 default[:ecto][:migrator][:install_path] = "/usr/local/bin"
